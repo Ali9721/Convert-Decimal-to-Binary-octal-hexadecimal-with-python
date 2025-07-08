5645070b24f6c84423075e5ecd52a582026753da
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-08 09:41:52 -0700

    Convert-Decimal-to-Binary-octal-hexadecimal
    1. Get input for number.
    
    2. then I define binary, octal, hexadecimal
    
    3. Output the result.

---
+++ Convert-Decimal-to-Binary-octal-hexadecimal-with-python.py
@@ -0,0 +1,12 @@
+# Get input for number.
+num = int(input("Please enter the number: "))
+
+# Now I define binary , octal, hexadecimal
+binary = bin(num)
+octal = oct(num)
+hexadecimal = hex(num)
+
+# Output the result.
+print(f"Binary:  {binary}")
+print(f"octal:  {octal}")
+print(f"hexadecimal:  {hexadecimal}")
